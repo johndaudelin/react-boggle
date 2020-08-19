4e152610ca8bb8a230f6452e81116182d89e17e4
John Daudelin <johndaudelin46@gmail.com>
2020-08-19 07:50:12 -0400

    Take out obsolete test code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
 import storeFactory from './store'
-// Change this to defaultState instead
-import { sampleState } from './constants'
+import { defaultState } from './constants'
 import { Provider } from 'react-redux'
-
-import { changeCurrentWord, addWord } from './actions'
 
 const initialState = localStorage['redux-store']
   ? JSON.parse(localStorage['redux-store'])
-  : sampleState
+  : defaultState
 
 const saveState = () => {
   const state = JSON.stringify(store.getState())
@@ -19,20 +16,7 @@ const saveState = () => {
 
 const store = storeFactory(initialState)
 
-// Take out these sample calls
 store.subscribe(saveState)
-
-store.dispatch(changeCurrentWord('eaten'))
-
-store.dispatch(addWord())
-
-store.dispatch(changeCurrentWord('dinosaur'))
-
-store.dispatch(addWord())
-
-store.dispatch(changeCurrentWord('cat'))
-
-store.dispatch(addWord())
 
 ReactDOM.render(
   <Provider store={store}>
