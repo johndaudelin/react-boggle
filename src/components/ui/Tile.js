6d5af178e621fdc1785f9583a26ff70d5be9e280
John Daudelin <johndaudelin46@gmail.com>
2020-08-20 12:54:28 -0400

    Improve boggle tile style and simplify code

--- src/components/ui/Tile.js
+++ src/components/ui/Tile.js
@@ -2,20 +2,10 @@ import React, { Component } from 'react'
 import '../../stylesheets/GameScreen.scss'
 
 export default class Tile extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      hovered: false
-    }
-  }
   render () {
     return (
       <div
-        className={`${
-          this.props.isClickable ? 'clickableTile' : 'disabledTile'
-        } 
-          ${this.state.hovered ? 'hoveredTile' : 'nonhoveredTile'} 
+        className={`${this.props.isClickable ? 'clickableTile' : 'disabledTile'}
           ${this.props.alreadyClicked ? 'clickedTile' : ''}
         `}
         onMouseEnter={() => this.setState({ hovered: true })}
